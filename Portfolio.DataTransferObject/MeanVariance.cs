21624da770fed313b5c01070ea84655fec95e12b
nielswart <niel.swart@live.co.za>
2013-02-05 18:28:51 +0200

    Added MeanVariance class

--- Portfolio.DataTransferObject/MeanVariance.cs
+++ Portfolio.DataTransferObject/MeanVariance.cs
@@ -7,12 +7,14 @@ using DataSciLib.DataStructures;
 namespace PortfolioEngine
 {
 
-
-
+    
+    /// <summary>
+    /// Represents the mean and variance-covariance of each variable (instrument)
+    /// </summary>
     public class MeanVariance
     {
-        // Add list of VItem's to represent mean return estimate
-
-        // Create a structure for covariance - list of (key value pairs - key:  pair of identifiers)
+        public string Symbol { get; set; }
+        public IEnumerable<ArrayItem<string, double>> Covariance { get; set; }
+        public double Mean { get; set; }
     }
 }
