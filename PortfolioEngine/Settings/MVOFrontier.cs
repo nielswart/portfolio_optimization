12cdab6f8d93019805321e126911c25d1a0f1890
nielswart <niel.swart@live.co.za>
2013-02-05 18:29:51 +0200

    Changed parameter types to use MeanVariane object

--- PortfolioEngine/Settings/MVOFrontier.cs
+++ PortfolioEngine/Settings/MVOFrontier.cs
@@ -18,41 +18,19 @@ namespace PortfolioEngine.Settings
     public class MVOFrontier
     {
         private PortfolioSettings _portfSet;
-        private CovarianceMatrix _covariance;
-        private SortedList<string, double> _meanReturns;
+        private List<MeanVariance> _meanVar;
 
         public Dictionary<int, ResultSet<double>> ResultsCollection { get; set; }
 
         /// <summary>
-        /// Create new mean-variance optimization object used for calculating the efficient frontier
+        /// 
         /// </summary>
         /// <param name="portfSet"></param>
-        /// <param name="mean"></param>
-        /// <param name="covariance"></param>
-        public MVOFrontier(PortfolioSettings portfSet, SortedList<string, double> meanReturns, double[,] covariance)
+        /// <param name="meanvar"></param>
+        public MVOFrontier(PortfolioSettings portfSet, List<MeanVariance> meanvar)
         {
             _portfSet = portfSet;
-            _meanReturns = meanReturns;
-            _covariance = new CovarianceMatrix(covariance);
-            
-            ResultsCollection = new Dictionary<int, ResultSet<double>>();
-            for (int i = 1; i < portfSet.Spec.NumberofFrontierPoints + 1; i++)
-            {
-                ResultsCollection.Add(i, new ResultSet<double>(i));
-            }
-        }
-
-        /// <summary>
-        ///  Create new mean-variance optimization object used for calculating the efficient frontier
-        /// </summary>
-        /// <param name="portfSet"></param>
-        /// <param name="mean"></param>
-        /// <param name="covariance"></param>
-        public MVOFrontier(PortfolioSettings portfSet, SortedList<string, double> meanReturns, CovarianceMatrix covariance)
-        {
-            _portfSet = portfSet;
-            _meanReturns = meanReturns;
-            _covariance = covariance;
+            _meanVar = meanvar;
 
             ResultsCollection = new Dictionary<int, ResultSet<double>>();
             for (int i = 1; i < portfSet.Spec.NumberofFrontierPoints + 1; i++)
@@ -78,27 +56,36 @@ namespace PortfolioEngine.Settings
             //         A'x >= b
             // meq = eqsumW
 
-            int numvars = _covariance.ColumnCount;
+            int numvars = _meanVar.Count;
 
             // calculate minimum variance portfolio
 
+            var meanReturns = from mv in _meanVar
+                              select mv.Mean;
+
+            var cov = from cv in _meanVar
+                      select cv.Covariance;
+
+            var covariance = CovarianceMatrix.Create(cov);
+
             // generate sequence of target returns for the efficient frontier locus
-            var targetReturns = new double[_portfSet.Spec.NumberofFrontierPoints].Seq(_meanReturns.Values.Min(), _meanReturns.Values.Max(), _portfSet.Spec.NumberofFrontierPoints);
+            var targetReturns = new double[_portfSet.Spec.NumberofFrontierPoints].Seq(meanReturns.Min(), meanReturns.Max(), _portfSet.Spec.NumberofFrontierPoints);
             for (int i = 0; i < _portfSet.Spec.NumberofFrontierPoints; i++)
             {
                 PerformanceLogger.Start("MVOFrontier", "Calculate", "PortfolioConfig");
-                var portfConf = new PortfolioConfig(_portfSet, _meanReturns, targetReturns[i]);
+                var portfConf = new PortfolioConfig(_portfSet, _meanVar, targetReturns[i]);
                 PerformanceLogger.Stop("MVOFrontier", "Calculate", "PortfolioConfig");
 
                 PerformanceLogger.Start("MVOFrontier", "Calculate", "QuadProg.Solve");
-                var result = QuadProg.Solve(_covariance, null, portfConf.Amat.Transpose(), portfConf.Bvec, portfConf.NumEquals);
+
+                var result = QuadProg.Solve(covariance, null, portfConf.Amat.Transpose(), portfConf.Bvec, portfConf.NumEquals);
                 PerformanceLogger.Stop("MVOFrontier", "Calculate", "QuadProg.Solve");
 
                 ResultsCollection[i + 1].Metrics[Metrics.Mean] = targetReturns[i];
                 ResultsCollection[i + 1].VectorMetrics[VMetrics.Weights] = result.Item1;
                 ResultsCollection[i + 1].Metrics[Metrics.Variance] = result.Item2;
                 ResultsCollection[i + 1].Metrics[Metrics.StdDev] = Math.Sqrt(result.Item2);
-                ResultsCollection[i + 1].MatrixMetrics[MatrixMetrics.CorrelationMatrix] = _covariance.ToCorrelation();
+                ResultsCollection[i + 1].MatrixMetrics[MatrixMetrics.CorrelationMatrix] = covariance.ToCorrelation();
             }       
         }
     }
