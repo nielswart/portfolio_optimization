7ad5c08d05418b80c26b800438c95168db0520e7
nielswart <niel.swart@live.co.za>
2013-02-05 18:35:54 +0200

    Reafctored Constraint factory methods; removed Rmetrics string code

--- PortfolioEngine/Settings/PortfolioConfig.cs
+++ PortfolioEngine/Settings/PortfolioConfig.cs
@@ -21,16 +21,10 @@ namespace PortfolioEngine.Settings
 
     internal class PortfolioConfig
     {
-        private List<Constraint> _constraints;
-        private SortedList<string, double> _meanReturns;
-        private SortedSet<string> _instruments;
+        private List<LinearConstraint> _constraints;
+        private List<MeanVariance> _meanVar;
+        private SortedSet<string> _universe;
 
-        /*
-        public StringBuilder ConstraintString { get; private set; }
-        public ModelInfo Model { get; private set; }
-        public PortfolioInfo Portfolio { get; private set; }
-        public SolverInfo Solver { get; private set; }
-         * */
         public DenseMatrix Amat { get; private set; }
         public double[] Bvec { get; private set; }
         public int NumEquals { get; private set; }
@@ -41,23 +35,15 @@ namespace PortfolioEngine.Settings
         /// <param name="portfSet"></param>
         /// <param name="expectedReturns"></param>
         /// <param name="targetReturn"></param>
-        public PortfolioConfig(PortfolioSettings portfSet, SortedList<string, double> expectedReturns, double targetReturn)
-        {
-            //ConstraintString = new StringBuilder();
-            
+        public PortfolioConfig(PortfolioSettings portfSet, List<MeanVariance> meanVar, double targetReturn)
+        {           
             int c=0;
-            _instruments = portfSet.Instruments;
-            _meanReturns = expectedReturns;
+            _universe = portfSet.Instruments;
+            _meanVar = meanVar;
 
             // Set constraint universe
-            Constraint.SetUniverse(_instruments);
-            _constraints = new List<Constraint>();
-
-            /*
-            Model = new ModelInfo();
-            Portfolio = new PortfolioInfo();
-            Solver = new SolverInfo();
-            */
+            LinearConstraint.SetUniverse(_universe);
+            _constraints = new List<LinearConstraint>();
 
             setConstraints(portfSet.Constr);
             setTargetReturnConstraints(targetReturn);
@@ -67,15 +53,9 @@ namespace PortfolioEngine.Settings
         public PortfolioConfig(PortfolioSettings portfSet)
         {
             //ConstraintString = new StringBuilder();
-            _constraints = new List<Constraint>();
-            _instruments = new SortedSet<string>();
-            _meanReturns = new SortedList<string, double>();
-
-            /*
-            Model = new ModelInfo();
-            Portfolio = new PortfolioInfo();
-            Solver = new SolverInfo();
-            */
+            _constraints = new List<LinearConstraint>();
+            _universe = new SortedSet<string>();
+           
             setConstraints(portfSet.Constr);
             extractConstraintMatrix();
         }
@@ -87,29 +67,18 @@ namespace PortfolioEngine.Settings
         private void setConstraints(OptimizationConstraints constr)
         {
             // Add base constraint -> all weights must sum to 1
-            var sl1 = new SortedList<string, double>();
-            foreach (var s in _instruments)
-            {
-                sl1.Add(s, 1);
-            }
-            _constraints.Add(Constraint.Create(sl1, Relational.Equal, 1));
+            _constraints.Add(LinearConstraint.Create(_universe, Relational.Equal, 1));
 
             // Add LongOnly constraint
             if (constr.SimpleConstraint != null)
             {
-                // Add Rmetrics string representation of constraint
-                //this.addStringConstraints((ConstraintType)constr.SimpleConstraint);
-
-                int index = 0;
                 if (constr.SimpleConstraint == ConstraintType.LongOnly)
                 {
-                    foreach (var s in _instruments)
+                    foreach (var s in _universe)
                     {
                         // No short positions allowed -> min = 0
-                        _constraints.Add(Constraint.Create(_instruments, Relational.Larger, 0, index));
-                        index++;
+                        _constraints.Add(LinearConstraint.Create(s, Relational.Larger, 0));
                     }
-                    
                 }
             }
 
@@ -120,58 +89,26 @@ namespace PortfolioEngine.Settings
                     setBoxConstraints(b.Assets, b.LowerLimit, b.UpperLimit);
                 }
             }
-
-            /*
-            if (constr.GroupConstr != null)
-            {
-                foreach (GroupConstraints g in constr.GroupConstr)
-                {
-                    setGroupConstraints(g.Assets, g.LowerLimit, g.UpperLimit);
-                }
-            }
-             * */
         }
 
-        private void setBoxConstraints(List<string> instruments, double lowerlimit = 0, double upperlimit = 1)
+        private void setBoxConstraints(HashSet<string> instruments, double lowerlimit = 0, double upperlimit = 1)
         {
-            /*
-            this.addMinWConstraints(instruments, lowerlimit);
-            this.addMaxWConstraints(instruments, upperlimit);
-            */
-            var sl = new SortedList<string, double>();
-            foreach (var s in _instruments)
-            {
-                sl.Add(s, 0);
-            }
-
-            // Add max and min weight constraints to list
-            foreach (var i in instruments)
-            {
-                sl[i] = 1;
-                _constraints.Add(Constraint.Create(sl, Relational.Smaller, upperlimit));
-                _constraints.Add(Constraint.Create(sl, Relational.Larger, lowerlimit));
-            }
+            _constraints.Add(LinearConstraint.Create(instruments, Relational.Smaller, upperlimit));
+            _constraints.Add(LinearConstraint.Create(instruments, Relational.Larger, lowerlimit));
         }
 
-        private void setGroupConstraints(List<string> group, double lowerlimit = 0, double upperlimit = 1, double? equal = null)
+        private void setGroupConstraints(HashSet<string> group, double lowerlimit = 0, double upperlimit = 1, double? equal = null)
         {
-            /*
-            if (equal == null)
-            {
-                this.addMinsumWConstraints(group, lowerlimit);
-                this.addMaxsumWConstraints(group, upperlimit);
-            }
-            else
-            {
-                this.addEqsumWConstraints(group, (double)equal);
-            }
-             * */
+            
         }
 
         private void setTargetReturnConstraints(double targetReturn)
         {
             // Sum of all weighted returns should be equal to the target return
-            _constraints.Add(Constraint.Create(_meanReturns, Relational.Equal, targetReturn));
+            var instList = from mr in _meanVar
+                           select new KeyValuePair<string, double>(mr.Symbol, mr.Mean);
+
+            _constraints.Add(LinearConstraint.Create(instList.ToDictionary(a => a.Key, b => b.Value), Relational.Equal, targetReturn));
         }
 
         private void extractConstraintMatrix()
@@ -179,7 +116,7 @@ namespace PortfolioEngine.Settings
             // Get the number of constraints
             var numcons = _constraints.Count();
             // Get the number of constrained variables
-            var numvars = _instruments.Count();
+            var numvars = _universe.Count();
 
             Amat = new DenseMatrix(new double[numcons, numvars]);
             Bvec = new double[numcons];
@@ -193,7 +130,7 @@ namespace PortfolioEngine.Settings
                 if (c.Relation == Relational.Equal)
                 {
                     numequals++;
-                    foreach (var w in c.Weights)
+                    foreach (var w in c.Coefficients)
                     {
                         Amat[i, j] = w.Value;
                         j++;
@@ -202,7 +139,7 @@ namespace PortfolioEngine.Settings
                 }
                 else if (c.Relation == Relational.Smaller)
                 {
-                    foreach (var w in c.Weights)
+                    foreach (var w in c.Coefficients)
                     {
                         Amat[i, j] = -w.Value;
                         j++;
@@ -211,7 +148,7 @@ namespace PortfolioEngine.Settings
                 }
                 else
                 {
-                    foreach (var w in c.Weights)
+                    foreach (var w in c.Coefficients)
                     {
                         Amat[i, j] = w.Value;
                         j++;
@@ -225,138 +162,69 @@ namespace PortfolioEngine.Settings
             NumEquals = numequals;
         }
 
-        #region Rmetrics string representation
-        /*
-        private void addStringConstraints(ConstraintType constr)
+        private class LinearConstraint : IComparable<LinearConstraint>
         {
-            ConstraintString.Append(",\"").Append(constr.ToString()).Append("\"");
-        }
-
-        private void addMinWConstraints(List<string> instruments, double lowerbound)
-        {
-            // "minW[<...>]=<...>"
-            ConstraintString.Append(",").Append("\"").Append("minW[c(\"");
-            foreach (string s in instruments)
-            {
-                ConstraintString.Append("\",\"").Append(s);
-            }
-            ConstraintString.Append("\")]=").Append(lowerbound).Append("\"");
-        }
-
-        private void addMaxWConstraints(List<string> instruments, double upperbound)
-        {
-            // "maxW[<...>]=<...>"
-            ConstraintString.Append(",").Append("\"").Append("maxW[c(\"");
-            foreach (string s in instruments)
-            {
-                ConstraintString.Append("\",\"").Append(s);
-            }
-            ConstraintString.Append("\")]=").Append(upperbound).Append("\"");
-        }
-
-        private void addEqsumWConstraints(List<string> group, double weight)
-        {
-            // "eqsumW[<...>]=<...>"
-            // e.g. "eqsumW[c(\"SPI\", \"SII\")]=0.6"
-            ConstraintString.Append(",").Append("\"").Append("eqsumW[c(\"");
-            foreach (string s in group)
-            {
-                ConstraintString.Append("\",\"").Append(s);
-            }
-            ConstraintString.Append("\")]=").Append(weight).Append("\"");
-        }
-
-        private void addMinsumWConstraints(List<string> group, double lowerbound)
-        {
-            // "minsumW[<...>]=<...>"
-            ConstraintString.Append(",").Append("\"").Append("minsumW[c(\"");
-            foreach (string s in group)
-            {
-                ConstraintString.Append("\",\"").Append(s);
-            }
-            ConstraintString.Append("\")]=").Append(lowerbound).Append("\"");
-        }
-
-        private void addMaxsumWConstraints(List<string> group, double upperbound)
-        {
-            // "maxsumW[<...>]=<...>"
-            ConstraintString.Append(",").Append("\"").Append("maxsumW[c(\"");
-            foreach (string s in group)
-            {
-                ConstraintString.Append("\",\"").Append(s);
-            }
-            ConstraintString.Append("\")]=").Append(upperbound).Append("\"");
-        }
-         * */
-        #endregion
-
-        #region Specification settings
-        /*
-        private void setSpecifications(PortfolioSpecification _spec)
-        {
-            // Implement rules for ensuring the correct solver is chosen
-            if (Model.Type == ModelType.MarkowitzMeanVariance && Solver.OptimizationObjective == Objective.MinimizeRisk)
-            {
-                // Solver type must be either QP or if unconstrained Analytic
-                Solver.Solver = SolverType.QP;  // Check constraints first
-            }
-            else if (Model.Type == ModelType.MarkowitzMeanVariance && Solver.OptimizationObjective == Objective.MaximizeReturn)
-            {
-            }
-        }
-        */
-        #endregion
-
-        private class Constraint : IComparable<Constraint>
-        {
-            public SortedList<string, double> Weights { get; private set; }
+            public SortedList<string, double> Coefficients { get; private set; }
             public double BValue { get; private set; }
             public Relational Relation { get; private set; }
-            public static SortedSet<string> Universe { get; set; }
-
-            private Constraint(SortedList<string, double> instruments, Relational relation, double value)
+            private static SortedSet<string> _universe { get; set; }
+            public SortedSet<string> Universe 
             {
-                Weights = instruments;
+                get { return _universe; }
+            }
+
+            private LinearConstraint(SortedList<string, double> instruments, Relational relation, double value)
+            {
+                Coefficients = instruments;
                 BValue = value;
                 Relation = relation;
             }
 
-            public static void SetUniverse(SortedList<string, double> instruments)
+            public static void SetUniverse(IEnumerable<string> instruments)
             {
-                var list = (IEnumerable<string>)instruments.Keys;
-                Universe = new SortedSet<string>(list);
+                _universe = new SortedSet<string>(instruments);
             }
 
             public static void SetUniverse(SortedSet<string> instruments)
             {
-                Universe = instruments;
+                _universe = instruments;
             }
 
-            public static Constraint Create(SortedList<string, double> instruments, Relational relation, double value)
-            {
-                return new Constraint(instruments, relation, value);
-            }
+            #region Create Constraints
 
-            
-            public static Constraint Create(SortedSet<string> instruments, Relational relation, double value, int index)
+            public static LinearConstraint Create(IDictionary<string, double> instruments, Relational relation, double value)
             {
-                if (index > instruments.Count)
-                    throw new ArgumentOutOfRangeException("The specifed index is out of range");
-
                 var sl = new SortedList<string, double>();
-                int c = 0;
-                foreach (var i in instruments)
+                foreach (var element in _universe)
                 {
-                    if (c == index)
-                        sl.Add(i, 1);
+                    if (instruments.ContainsKey(element))
+                        sl.Add(element, instruments[element]);
                     else
-                        sl.Add(i, 0);
-                    c++;
+                        sl.Add(element, 0);
                 }
-                return new Constraint(sl, relation, value);
+                return new LinearConstraint(sl, relation, value);
             }
 
-            public int CompareTo(Constraint constrB)
+            public static LinearConstraint Create(IEnumerable<string> instruments, Relational relation, double value)
+            {
+                // for every instrument that is also in the universe, set matrix coeffcient to 1, for all others in universe set to 0
+                var sl = new SortedList<string, double>();
+                foreach (var element in _universe)
+                {
+                    if (instruments.Contains(element))
+                        sl.Add(element, 1);
+                    else
+                        sl.Add(element, 0);
+                }
+                return new LinearConstraint(sl, relation, value);
+            }
+
+            public static LinearConstraint Create(string instrument, Relational relation, double value)
+            {
+                throw new NotImplementedException();
+            }
+
+            public int CompareTo(LinearConstraint constrB)
             {
                 if (this.Relation != Relational.Equal && constrB.Relation == Relational.Equal)
                 {
@@ -367,6 +235,8 @@ namespace PortfolioEngine.Settings
                 else
                     return 0;
             }
+
+            #endregion
         }
     }
 }
