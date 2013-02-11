c9f9899e8190a9f5172f9a76833e2ef40e2b8c33
nielswart <niel.swart@live.co.za>
2013-02-11 10:40:01 +0200

    Added new IMultiTimeSeries interface and an implementation thereof - Datastructure for multivariate time series

---
+++ DataSciLib/DataStructures/TimeSeries/MultiTimeSeries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataSciLib.DataStructures.TimeSeries
+{
+    public interface IMultiTimeSeries<T> : ITimeSeries<T> 
+    {
+        ITimeSeries<T> this[string name] { get; }
+
+        ITimeSeries<T> this[string[] names] { get; }
+
+        string[] Names { get; }
+
+        T this[DateTime date, string name] { get; }
+
+        ITimeSeries<T> this[DateTime[] dates, string[] names] { get; }
+
+        void ColumnBind(ITimeSeries<T> timeseries);
+
+        int ColumnCount { get; }
+    }
+
+    public class MultiTimeSeries<T> : IMultiTimeSeries<T>, IEnumerable<TimeSeriesItem<T>> 
+    {
+
+    }
+}
