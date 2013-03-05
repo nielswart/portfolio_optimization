5134212cb4ae6ff942f98a398155f0549dac52de
nielswart <niel.swart@live.co.za>
2013-03-05 12:54:43 +0200

    Simplified EfficientFrontier interface

--- PortfolioEngine/Portfolios/EfficientFrontier.cs
+++ PortfolioEngine/Portfolios/EfficientFrontier.cs
@@ -9,30 +9,16 @@ using PerformanceTools;
 
 namespace PortfolioEngine.Portfolios
 {
-    /// <summary>
-    /// Represents the total efficient frontier
-    /// Implements IPortfolio interface
-    /// </summary>
-    [CollectionDataContract]
-    public sealed class EfficientFrontier : PortfolioCollection
+    public static class EfficientFrontier
     {
-        public EfficientFrontier()
+        public static IPortfolioCollection CalculateMVFrontier(this IPortfolioCollection portfCol,  IPortfolio samplePortf, uint numPortfolios)
         {
-            
-        }
+            //PerformanceLogger.Start("EfficientFrontier", "CalculateMVFrontier", "MVOFrontier.Calculate");
+            var mvfrontier = new MVOFrontier(samplePortf, numPortfolios);
+            var portfCollection = mvfrontier.Calculate();
+            //PerformanceLogger.Stop("EfficientFrontier", "CalculateMVFrontier", "MVOFrontier.Calculate");
 
-        public static IPortfolioCollection CalculateMVFrontier(PortfolioSettings portfset, List<MeanVariance> meanVar)
-        {
-            PerformanceLogger.Start("EfficientFrontier", "CalculateMVFrontier", "MVOFrontier.Calculate");
-            var mvfrontier = new MVOFrontier(portfset, meanVar);
-            mvfrontier.Calculate();
-            PerformanceLogger.Stop("EfficientFrontier", "CalculateMVFrontier", "MVOFrontier.Calculate");
-
-            PerformanceLogger.Start("EfficientFrontier", "CalculateMVFrontier", "PortfolioCollection.AddMetrics");
-            var portfCollection = new PortfolioCollection();
-            portfCollection.AddMetrics(mvfrontier.ResultsCollection);
-            PerformanceLogger.Stop("EfficientFrontier", "CalculateMVFrontier", "PortfolioCollection.AddMetrics");
-            return portfCollection;
+            return (IPortfolioCollection)portfCollection;
         }
     }
 }
