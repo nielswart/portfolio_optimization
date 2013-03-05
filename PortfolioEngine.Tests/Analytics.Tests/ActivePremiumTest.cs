b0cb946dba3db11e1b1f7e85fcf84ab3b91cc91f
nielswart <niel.swart@live.co.za>
2013-03-05 16:34:03 +0200

    Moved portfolio Analytics Test into their own folder

---
+++ PortfolioEngine.Tests/Analytics.Tests/ActivePremiumTest.cs
@@ -0,0 +1,20 @@
+﻿using DataSciLib.DataStructures;
+using DataSciLib.REngine;
+using MathNet.Numerics;
+using MathNet.Numerics.Distributions;
+using MathNet.Numerics.Statistics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PortfolioEngine;
+using PortfolioEngine.Portfolios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PortfolioEngine.Tests
+{
+    [TestClass]
+    public class ActivePremiumTest
+    {
+    }
+}
