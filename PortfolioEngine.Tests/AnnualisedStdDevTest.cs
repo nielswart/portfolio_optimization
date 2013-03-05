7c956d09cd07d6f4a84b9c3ef17826d489170b36
nielswart <niel.swart@live.co.za>
2013-03-05 12:50:48 +0200

    Added new AnnualisedStdDev unit test

---
+++ PortfolioEngine.Tests/AnnualisedStdDevTest.cs
@@ -0,0 +1,52 @@
+﻿using DataSciLib.DataStructures;
+using DataSciLib.REngine;
+using MathNet.Numerics;
+using MathNet.Numerics.Distributions;
+using MathNet.Numerics.Statistics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PortfolioEngine;
+using PortfolioEngine.Portfolios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PortfolioEngineLib.Tests
+{
+    [TestClass]
+    public class AnnualisedStdDevTest
+    {
+        const int elements = 100;
+        const double mu = 0.01;
+        const double sigma = 0.02;
+        const int seed = 5;
+
+        ITimeSeries<double> monthlySampleTS = TimeSeriesFactory<double>.SampleData.Gaussian(mu, sigma, numperiods: elements, freq: DataFrequency.Monthly);
+        ITimeSeries<double> dailySampleTS = TimeSeriesFactory<double>.SampleData.Gaussian(mu, sigma, numperiods: elements, freq: DataFrequency.Daily);
+
+        [TestMethod]
+        public void NormalOperation()
+        {
+            var res = PortfolioEngine.Analytics.AnnualisedStdDev(TimeSeriesFactory<double>.SampleData.Gaussian(0, 0.2, 100));
+            // Check output type
+            Assert.IsNotNull(res);
+
+            double delta = 0.001;
+
+            // Data
+            int elements = 100;
+            TimeSeriesFactory<double>.SampleData.RandomSeed = 5;
+
+            // Monthly data test
+            var returns1 = Normal.Samples(new Random(5), 0.01, 0.02).Take(elements);
+            var sdm = PortfolioEngine.Analytics.AnnualisedStdDev(monthlySampleTS);
+            Assert.AreEqual(Statistics.StandardDeviation(returns1) * Math.Sqrt(12), sdm, delta);
+
+            // Daily data test
+            var sdd = PortfolioEngine.Analytics.AnnualisedStdDev(dailySampleTS);
+            var returns2 = Normal.Samples(new Random(5), 0.01, 0.02).Take(elements);
+            Assert.AreEqual(Statistics.StandardDeviation(returns2) * Math.Sqrt(252), sdd, delta);
+        }
+
+    }
+}
