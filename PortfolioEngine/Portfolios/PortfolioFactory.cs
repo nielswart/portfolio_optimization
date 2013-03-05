c56a843de7a249cee8cade3cbc61d625c3569f98
nielswart <niel.swart@live.co.za>
2013-03-05 12:53:08 +0200

    Removed deprecated methods from PortfolioFactory

--- PortfolioEngine/Portfolios/PortfolioFactory.cs
+++ PortfolioEngine/Portfolios/PortfolioFactory.cs
@@ -7,86 +7,20 @@ using DataSciLib.DataStructures;
 
 namespace PortfolioEngine.Portfolios
 {
-    public enum Rebalance
-    {
-        Never,
-        Continuous,
-        Daily,
-        Weekly,
-        Monthly,
-        Quarterly,
-        SemiAnnually,
-        Annually
-    }
-
     public class PortfolioFactory
     {
-        public static IPortfolio Create(string ID)
+        public static IPortfolio Create(IPortfolio samplePortfolio, string name, double mean, double variance)
         {
-            //GeneralTargetPortfolio portf = new GeneralTargetPortfolio(
-            throw new NotImplementedException();
+            Portfolio portf = new Portfolio(name, mean, variance);
+
+            portf.AddRange(samplePortfolio);
+            portf.SetConstraints(samplePortfolio.Constraints);
+            return portf;
         }
+    }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="data"></param>
-        /// <param name="weights"></param>
-        /// <returns></returns>
-        public static IPortfolio Create(IEnumerable<Tuple<ITimeSeries<double>,double>> weightsData, Rebalance rebalance = Rebalance.Never)
-        {
-            var gentp = new GeneralPortfolio();
+    public static class PortfolioTimeSeriesExtensions
+    {
 
-            switch (rebalance)
-            {
-                case Rebalance.Never:
-                    {
-                        /*
-                        foreach (var ts in data)
-                        {
-
-                        }
-                         * */
-                    }
-                    break;
-            }
-            // Calculate PortfolioValue timeseries
-            return gentp;
-        }
-
-        public static IPortfolio Create(ITimeSeries<double> data, IEnumerable<double> weights, DateTime startdate, DateTime enddate, Rebalance rebalance = Rebalance.Never)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static IPortfolio Create(PortfolioSettings portfset, ITimeSeries<double> data)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static IPortfolio Create(MetricsCollection<double> results)
-        {
-            var gentp = new GeneralPortfolio();
-
-            gentp.Metrics = results.Metrics;
-            gentp.VectorMetrics = results.VectorMetrics;
-            //gentp.Weights = results.VectorMetrics[VMetrics.Weights];
-            gentp.MatrixMetrics = results.MatrixMetrics;
-
-            // Calculate PortfolioValue timeseries
-            return gentp;
-        }
-
-        public static IPortfolio Create(MetricsCollection<double> results, int id)
-        {
-            var gentp = new GeneralPortfolio(id.ToString());
-
-            gentp.Metrics = results.Metrics;
-            gentp.VectorMetrics = results.VectorMetrics;
-            //gentp.Weights = results.VectorMetrics[VMetrics.Weights];
-            gentp.MatrixMetrics = results.MatrixMetrics;
-
-            return gentp;
-        }
     }
 }
