1afd7c4034961dff054765e88913d61cd7692181
nielswart <niel.swart@live.co.za>
2013-03-07 22:23:53 +0200

    Added native .NET CAPModel

--- PortfolioEngine/Analytics.cs
+++ PortfolioEngine/Analytics.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) 2012: DJ Swart, AJ Hoffman
 //
 
+using DataSciLib.Statistics;
 using DataSciLib.DataStructures;
 using DataSciLib.REngine;
 using DataSciLib.REngine.PerformanceAnalytics;
@@ -13,21 +14,18 @@ using System.Linq;
 
 namespace PortfolioEngine
 {
-    public class Result<Tval> : ArrayItem<string, Tval>
+    /// <summary>
+    /// 
+    /// </summary>
+    public struct CAPMCoefficients
     {
-        public string ID
-        {
-            get { return _id; }
-        }
+        public readonly double Alpha;
+        public readonly double Beta;
 
-        public Tval Value
+        public CAPMCoefficients(double alpha, double beta)
         {
-            get { return _value; }
-        }
-
-        public Result(string id, Tval value)
-            : base(id, value)
-        {
+            this.Alpha = alpha;
+            this.Beta = beta;
         }
     }
 
@@ -55,31 +53,47 @@ namespace PortfolioEngine
         #endregion
 
         /// <summary>
-        /// Calculate the annualised return of the portfolio returns
+        /// Calculates the annualised return of the given time series
         /// </summary>
-        /// <param name="timeseries"></param>
-        /// <returns></returns>
+        /// <param name="timeseries">A time series of returns</param>
+        /// <returns>Annualised return in the same units as the <paramref name="timeseries"/> parameter </returns>
         public static double AnnualisedReturn(ITimeSeries<double> timeseries)
         {
             return timeseries.AnnualisedMean();
         }
 
         /// <summary>
-        /// Calculate the anualised standard deviation of the portfolio returns
+        /// Calculates the anualised standard deviation of the given time series
         /// </summary>
-        /// <param name="timeseries"></param>
-        /// <returns></returns>
+        /// <param name="timeseries">A time series of returns</param>
+        /// <returns>Annualised return in the same units as the <paramref name="timeseries"/> parameter</returns>
         public static double AnnualisedStdDev(ITimeSeries<double> timeseries)
         {
             return timeseries.AnnualisedStdDev();
         }
 
         /// <summary>
-        /// Calculate the realised alpha with the same periodicity as the returns
+        /// Returns the regression coefficients and the residual vector of the linear CAPM regresion model
+        /// </summary>
+        /// <param name="asset"></param>
+        /// <param name="benchmark"></param>
+        /// <param name="riskfree"></param>
+        /// <returns>Tuple with the following elements - alpha, beta and residual vector of the same size as the asset and benchmark time series</returns>
+        public static CAPMCoefficients CAPModel(ITimeSeries<double> asset, ITimeSeries<double> benchmark, double riskfree = 0)
+        {
+            var Ra = asset.ToTimeSeries() - riskfree;
+            var Rb = benchmark.ToTimeSeries() - riskfree;
+            
+            var capm = Model.LinearRegression(Ra.Data, Rb.Data);
+            return new CAPMCoefficients(capm.Intercept, capm.Coefficients.First());
+        }
+
+        /// <summary>
+        /// Calculate the annualised realised alpha from a CAPM linear regression model
         /// </summary>
         /// <param name="asset">The asset time series to use in calculating alpha</param>
         /// <param name="benchmark">The benchmark index/portfolio to calculate alpha against</param>
-        /// <param name="riskfree">The risk-free rate with the same periodicity as the returns</param>
+        /// <param name="riskfree">The annualised risk-free rate with the same periodicity as the returns</param>
         /// <returns>Array of alphas, one for each series in asset time series</returns>
         public static double RealisedAlpha(ITimeSeries<double> asset, ITimeSeries<double> benchmark, double riskfree = 0)
         {
@@ -87,10 +101,12 @@ namespace PortfolioEngine
             if (riskfree >= 1 || riskfree < 0)
                 throw new ArgumentException("The risk-free rate should be a value between 0 and 1");
 
+            // Find a better way of comparing two series for equivalence - time and length (maybe extract part of benchmark that is comparable to asset ??
             if (asset.RowCount != benchmark.RowCount)
                 throw new ArgumentException("TimeSeries should be of the same length");
 
-            return CAPM.Beta(timeSeries.Create(asset), timeSeries.Create(benchmark), riskfree).First();
+            return CAPModel(asset, benchmark, riskfree).Alpha;
+            //return CAPM.Beta(timeSeries.Create(asset), timeSeries.Create(benchmark), riskfree).First();
         }
 
         /// <summary>
@@ -109,7 +125,8 @@ namespace PortfolioEngine
             if (asset.RowCount != benchmark.RowCount)
                 throw new ArgumentException("Time series should be of the same length");
 
-            return CAPM.Beta(timeSeries.Create(asset), timeSeries.Create(benchmark), riskfree).First();
+            return CAPModel(asset, benchmark, riskfree).Beta;
+            //return CAPM.Beta(timeSeries.Create(asset), timeSeries.Create(benchmark), riskfree).First();
         }
 
         public static double MarginalReturn(IPortfolio portfolio)
@@ -124,7 +141,10 @@ namespace PortfolioEngine
 
         public static double ResidualRisk(ITimeSeries<double> asset, ITimeSeries<double> benchmark, double riskfree = 0)
         {
-            throw new NotImplementedException();
+            var Ra = asset.ToTimeSeries() - riskfree;
+            var Rb = benchmark.ToTimeSeries() - riskfree;
+
+            return Statistics.StandardDeviation(Model.LinearRegression(Ra.Data, Rb.Data).Residuals);
         }
 
         public static double SharpeRatio(ITimeSeries<double> portfolio, double riskFreeRate = 0)
