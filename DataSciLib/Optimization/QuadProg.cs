f86e33560e309ced952b8ed328bf3d2c695570c5
nielswart <niel.swart@live.co.za>
2013-03-27 18:12:52 +0200

    Implemented and tested .NET only Goldfarb-Idnani quadratic optimization solver

--- DataSciLib/Optimization/QuadProg.cs
+++ DataSciLib/Optimization/QuadProg.cs
@@ -3,33 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Accord.Math.Optimization;
+using MathLib = MathNet.Numerics.LinearAlgebra.Generic;
+using MathNet.Numerics.LinearAlgebra.Double;
+using PerformanceTools;
 
 namespace DataSciLib.Optimization
 {
+    public struct OptimizationResult
+    {
+        public readonly double[] Solution;
+        public readonly double Value;
+
+        public OptimizationResult(double[] sol, double val)
+        {
+            Solution = sol;
+            Value = val;
+        }
+    }
+
     public class QuadProg
     {
         /// <summary>
-        /// quadratic programming optimization solver using the dual method algorithm of Goldfarb and Idnani 
+        /// Quadratic programming optimization solver using the dual method algorithm of Goldfarb and Idnani 
         /// </summary>
         /// <param name="Dmat"></param>
         /// <param name="dvec"></param>
         /// <param name="Amat"></param>
         /// <param name="bvec"></param>
         /// <param name="meq"></param>
-        public static Tuple<double[], double> Solve(double[,] Q, double[] d, double[,] Amat, double[] bvec, int numeq)
+        public static OptimizationResult Solve(MathLib.Matrix<double> Dmat, double[] dvec, MathLib.Matrix<double> Amat, double[] bvec, int meq = 0, bool factorized = false)
         {
-            try
-            {
-                // return optim = optim, weights= weights, targetReturn = bvec[1],  targetRisk = sqrt(weights*Dmat*weights)[[1,1]]
-                var target = new GoldfarbIdnaniQuadraticSolver(Q.GetLength(0), Amat, bvec, numeq);
-                var min = target.Minimize(Q, d);
-                var sol = target.Solution;
-                return new Tuple<double[],double>(sol, min);
-            }
-            catch 
-            {
-                return null;
-            }
+            // return optim = optim, weights= weights, targetReturn = bvec[1],  targetRisk = sqrt(weights*Dmat*weights)[[1,1]]
+            var target = new GoldfarbIdnaniQuadraticSolver(Dmat.RowCount, Amat.ToArray(), bvec, meq);
+            var min = target.Minimize(Dmat.ToArray(), dvec);
+            var sol = target.Solution;
+            return new OptimizationResult(sol, min*2);
         }
     }
 }
